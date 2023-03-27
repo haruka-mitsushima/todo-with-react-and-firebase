c0952b079aff29b340338a227889e264319dc6b5
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-03-27 11:49:18 +0900

    Material-UIの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      こんにちはHello
+      <Button variant="contained">Hello</Button>
+    </div>
+  );
 }
 
 export default App;
