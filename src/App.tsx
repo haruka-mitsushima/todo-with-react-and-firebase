109c4e60de90058003dc1c5a3658433514444750
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-03-27 11:57:52 +0900

    ルーティングの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import "./App.css";
-import Button from "@mui/material/Button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Done from "./components/Done";
+import CreateTask from "./components/CreateTask";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
   return (
-    <div>
-      こんにちはHello
-      <Button variant="contained">Hello</Button>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/done" element={<Done />}></Route>
+        <Route path="/create" element={<CreateTask />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/logout" element={<Logout />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
