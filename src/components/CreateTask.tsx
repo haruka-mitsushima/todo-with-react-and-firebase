4d5d9797779a0c9f7f9261950808dde04f95730f
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-03-28 11:53:49 +0900

    MUI追加、CSS編集

--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,17 +1,65 @@
-import React, { useEffect, useState } from "react";
+import React, { FormEvent, useEffect, useState } from "react";
 import "../styles/CreateTask.css";
 import { addDoc, collection } from "firebase/firestore";
 import { auth, db } from "../firebase";
 import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import { Theme, useTheme } from "@mui/material/styles";
+
+const tags = ["仕事", "家事", "緊急"];
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+function getStyles(name: string, personName: readonly string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
 
 const CreateTask = () => {
+  const theme = useTheme();
   const isAuth = sessionStorage.getItem("uid");
-  const [title, setTitle] = useState("");
-  const [taskText, setTaskText] = useState("");
+  const [tagName, setTagName] = useState<string[]>([]);
 
   const navigate = useNavigate();
 
-  const createTask = async () => {
+  const handleChange = (event: SelectChangeEvent<typeof tagName>) => {
+    const {
+      target: { value },
+    } = event;
+    setTagName(
+      // On autofill we get a stringified value.
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    const task = data.get("task")?.toString();
+    const detail = data.get("detail")?.toString();
+    const tags = data.get("tag")?.toString().split(",");
     // await addDoc(collection(db, "tasks"), {
     //   title,
     //   tasksText: taskText,
@@ -20,7 +68,8 @@ const CreateTask = () => {
     //     id: auth.currentUser.uid,
     //   },
     // });
-    navigate("/");
+    console.log({ task, detail, tags });
+    // navigate("/");
   };
 
   useEffect(() => {
@@ -31,35 +80,96 @@ const CreateTask = () => {
 
   return (
     <div className="createTaskPage">
-      <div className="taskContainer">
-        <h1>タスクを追加する</h1>
-        <div className="inputTask">
-          <div>タスク</div>
-          <input
-            type="text"
-            placeholder="タスクを記入"
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div className="inputTask">
-          <div>説明</div>
-          <textarea
-            placeholder="説明を記入"
-            onChange={(e) => setTaskText(e.target.value)}
-          ></textarea>
-        </div>
-        <div className="selectTag">
-          <div>タグ</div>
-          <select name="tag">
-            <option value="仕事">仕事</option>
-            <option value="家事">家事</option>
-            <option value="緊急">緊急</option>
-          </select>
-        </div>
-        <button className="taskButton" onClick={createTask}>
-          追加する
-        </button>
-      </div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "white",
+          borderRadius: 10,
+          py: 0,
+          px: 4,
+          height: 450,
+          width: 500,
+          boxShadow: 10,
+        }}
+      >
+        <Typography component="h1" variant="h5">
+          タスクを追加する
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="task"
+                label="タスク"
+                name="task"
+                // autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="detail"
+                label="詳細"
+                name="detail"
+                // autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl sx={{ width: 500 }}>
+                <InputLabel id="tag">タグ</InputLabel>
+                <Select
+                  labelId="tag"
+                  id="tag"
+                  name="tag"
+                  multiple
+                  value={tagName}
+                  onChange={handleChange}
+                  input={
+                    <OutlinedInput id="select-multiple-chip" label="Chip" />
+                  }
+                  renderValue={(selected) => (
+                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  )}
+                  MenuProps={MenuProps}
+                >
+                  {tags.map((tag) => (
+                    <MenuItem
+                      key={tag}
+                      value={tag}
+                      style={getStyles(tag, tagName, theme)}
+                    >
+                      {tag}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              bgcolor: "#4970a3",
+              ":hover": { background: "#3b5a84" },
+            }}
+          >
+            登録する
+          </Button>
+        </Box>
+      </Box>
     </div>
   );
 };
