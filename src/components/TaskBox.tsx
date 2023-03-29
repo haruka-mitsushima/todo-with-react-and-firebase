88f316197d095cdcf6f0df1b85fa8b778a52d356
Haruka Mitsushima <haruka-mitsushima@rakus-partners.co.jp>
2023-03-29 16:25:12 +0900

    タスクの完了、削除機能の実装

--- src/components/TaskBox.tsx
+++ src/components/TaskBox.tsx
@@ -7,72 +7,177 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
 import Tags from "./Tags";
+import {
+  doc,
+  query,
+  collection,
+  where,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
+import { useDispatch } from "react-redux";
+import { setTasks } from "../features/task/TaskSlice";
 
 const TaskBox = ({ task }: { task: Task }) => {
+  const dispatch = useDispatch();
+
+  const getTasks = async () => {
+    const q = query(
+      collection(db, "tasks"),
+      where("userId", "==", sessionStorage.getItem("uid")),
+      where("deleted", "==", false)
+    );
+    const data = await getDocs(q);
+    console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    const tasks = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
+    dispatch(setTasks(tasks));
+  };
+
+  const setDone = async (id: string, done: boolean) => {
+    await updateDoc(doc(db, "tasks", id), { done: !done });
+    getTasks();
+  };
+
+  const deleteTask = async (id: string) => {
+    console.log("削除");
+    await updateDoc(doc(db, "tasks", id), { deleted: true });
+    getTasks();
+  };
+
+  console.log("レンダーされました");
   return (
     <div className="taskBox">
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "start",
-          bgcolor: "white",
-          borderRadius: 3,
-          py: 0,
-          px: 1,
-          height: 90,
-          width: 500,
-          boxShadow: 10,
-          m: 3,
-        }}
-      >
-        <Grid container sx={{ display: "flex", alignItems: "center" }}>
-          <Grid item xs={1}>
-            <Checkbox
-              value={task.id}
-              sx={{
-                color: "#4970a3",
-                "&.Mui-checked": {
-                  color: "#d32f2f",
-                },
-              }}
-            />
-          </Grid>
-          {/* <div className="task"> */}
-          <Grid item xs={7} sx={{ pl: 2 }}>
-            <Typography component="h1" sx={{ fontSize: 18, fontWeight: 900 }}>
-              {task.task}
-            </Typography>
-            <Typography component="h2" variant="subtitle2">
-              {task.detail}
-            </Typography>
-          </Grid>
-          <Grid item xs={2}>
-            {task.tags.length > 0 &&
-              task.tags.map((tag) => <Tags tag={tag} key={tag} />)}
-          </Grid>
-          {/* <div className="name">{task.task}</div> */}
-          {/* <div className="detail">{task.detail}</div> */}
-          {/* </div> */}
-          <Grid item xs={2}>
-            {/* <div className="btn"> */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{
-                mt: 3,
-                mb: 2,
-                bgcolor: "#4970a3",
-                ":hover": { background: "#3b5a84" },
-              }}
-            >
-              削除
-            </Button>
+      {!task.done ? (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "start",
+            bgcolor: "white",
+            borderRadius: 3,
+            py: 0,
+            px: 1,
+            height: 90,
+            width: 500,
+            boxShadow: 10,
+            m: 3,
+          }}
+        >
+          <Grid container sx={{ display: "flex", alignItems: "center" }}>
+            <Grid item xs={1}>
+              <Checkbox
+                onClick={() => setDone(task.id, task.done)}
+                defaultChecked
+                value={task.id}
+                sx={{
+                  color: "#4970a3",
+                  "&.Mui-checked": {
+                    color: "#d32f2f",
+                  },
+                }}
+              />
+            </Grid>
+            {/* <div className="task"> */}
+            <Grid item xs={7} sx={{ pl: 2 }}>
+              <Typography component="h1" sx={{ fontSize: 18, fontWeight: 900 }}>
+                {task.task}
+              </Typography>
+              <Typography component="h2" variant="subtitle2">
+                {task.detail}
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              {task.tags.length > 0 &&
+                task.tags.map((tag) => <Tags tag={tag} key={tag} />)}
+            </Grid>
+            {/* <div className="name">{task.task}</div> */}
+            {/* <div className="detail">{task.detail}</div> */}
             {/* </div> */}
+            <Grid item xs={2}>
+              {/* <div className="btn"> */}
+              <Button
+                onClick={() => deleteTask(task.id)}
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  bgcolor: "#4970a3",
+                  ":hover": { background: "#3b5a84" },
+                }}
+              >
+                削除
+              </Button>
+              {/* </div> */}
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "start",
+            bgcolor: "white",
+            borderRadius: 3,
+            py: 0,
+            px: 1,
+            height: 90,
+            width: 500,
+            boxShadow: 10,
+            m: 3,
+          }}
+        >
+          <Grid container sx={{ display: "flex", alignItems: "center" }}>
+            <Grid item xs={1}>
+              <Checkbox
+                onClick={() => setDone(task.id, task.done)}
+                value={task.id}
+                sx={{
+                  color: "#4970a3",
+                  "&.Mui-checked": {
+                    color: "#d32f2f",
+                  },
+                }}
+              />
+            </Grid>
+            {/* <div className="task"> */}
+            <Grid item xs={7} sx={{ pl: 2 }}>
+              <Typography component="h1" sx={{ fontSize: 18, fontWeight: 900 }}>
+                {task.task}
+              </Typography>
+              <Typography component="h2" variant="subtitle2">
+                {task.detail}
+              </Typography>
+            </Grid>
+            <Grid item xs={2}>
+              {task.tags.length > 0 &&
+                task.tags.map((tag) => <Tags tag={tag} key={tag} />)}
+            </Grid>
+            {/* <div className="name">{task.task}</div> */}
+            {/* <div className="detail">{task.detail}</div> */}
+            {/* </div> */}
+            <Grid item xs={2}>
+              {/* <div className="btn"> */}
+              <Button
+                onClick={() => deleteTask(task.id)}
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  bgcolor: "#4970a3",
+                  ":hover": { background: "#3b5a84" },
+                }}
+              >
+                削除
+              </Button>
+              {/* </div> */}
+            </Grid>
+          </Grid>
+        </Box>
+      )}
     </div>
   );
 };
--- src/features/task/TaskSlice.ts
+++ src/features/task/TaskSlice.ts
@@ -1,19 +1,36 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../firebase";
 import { Tasks } from "../../type";
 
-const initialState: { tasks: Tasks } = { tasks: [] }
+let tasks: Tasks = []
+
+const getTasks = async () => {
+  const q = query(
+    collection(db, "tasks"),
+    where("userId", "==", sessionStorage.getItem("uid"))
+  );
+  const data = await getDocs(q);
+  console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  const result = data.docs.map((doc) => ({ id: doc.id, task: doc.data().task, detail: doc.data().detail, tags: doc.data().tags, done: doc.data().done, deleted: doc.data().deleted, userId: doc.data().uid, }));
+  tasks = result
+};
+
+getTasks()
+
+const initialState: { tasks: Tasks } = { tasks: tasks }
 
 const taskSlice = createSlice({
   name: "task",
   initialState,
   reducers: {
-    getAll: (state, action) => {
+    setTasks: (state, action) => {
       state.tasks = action.payload
     }
   }
 })
 
 
-export const { getAll } = taskSlice.actions
+export const { setTasks } = taskSlice.actions
 
 export default taskSlice.reducer
